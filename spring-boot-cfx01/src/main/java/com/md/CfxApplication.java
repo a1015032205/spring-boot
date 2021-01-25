17ff7703192aa06fc137f9d008d81c23ce28ff97
a1015032205 <1015032205@qq.com>
2021-01-25 21:51:37 +0800

    win

--- spring-boot-cfx01/src/main/java/com/md/CfxApplication.java
+++ spring-boot-cfx01/src/main/java/com/md/CfxApplication.java
@@ -1,13 +1,13 @@
-package com.md.cfx;
+package com.md;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author 秒度
- * @Email fangxin.md@Gmail.com
- * @Date 2021/1/24 16:00
- * @Description
+ * @Author: 秒度
+ * @Email: fangxin.md@Gmail.com
+ * @Date: 2021-01-25 20:57
+ * @Description:
  */
 @SpringBootApplication
 public class CfxApplication {
